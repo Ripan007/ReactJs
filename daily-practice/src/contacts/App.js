87904c9f9a3854a99f102b40c728048c41dd3f9d
Ripan Mondal <92440897+Ripan007@users.noreply.github.com>
2022-11-07 23:20:29 +0530

    add

--- daily-practice/src/contacts/App.js
+++ daily-practice/src/contacts/App.js
@@ -4,16 +4,29 @@ import contacts from './contacts';
 import Avatar from './Avatar';
 import './Avatar.css';
 
+function createCard(contact) {
+    return (
+        <Card
+            key={contact.id}
+            name={contact.name}
+            phoneNo={contact.phoneNo}
+            mail={contact.mail}
+            image={contact.imageUrl}
+        />
+    )
+
+}
+
 function App() {
     return (
         <div>
             <Avatar image="https://images.pexels.com/photos/1468379/pexels-photo-1468379.jpeg?auto=compress&cs=tinysrgb&w=600" />
 
             <h1>My contacts</h1>
-            <Card name={contacts[0].name} image={contacts[0].imageUrl} phoneNo={contacts[0].contactNo} mail={contacts[0].mail} />
+            {/* <Card name={contacts[0].name} image={contacts[0].imageUrl} phoneNo={contacts[0].contactNo} mail={contacts[0].mail} />
             <Card name={contacts[1].name} image={contacts[1].imageUrl} phoneNo={contacts[1].contactNo} mail={contacts[1].mail} />
-            <Card name={contacts[2].name} image={contacts[2].imageUrl} phoneNo={contacts[2].contactNo} mail={contacts[2].mail} />
-
+            <Card name={contacts[2].name} image={contacts[2].imageUrl} phoneNo={contacts[2].contactNo} mail={contacts[2].mail} /> */}
+            {contacts.map(createCard)}
 
         </div>
 
